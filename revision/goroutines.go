6693b866848efef077ece51866b97aa9ae74bdeb
Anre Pretorius <170444398+AnrePre@users.noreply.github.com>
2025-06-16 17:26:18 +0200

    goroutines revision

---
+++ revision/goroutines.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	var err error
+
+	fmt.Println("Beginning program.")
+	go sayHello()
+	fmt.Println("After sayHello function.")
+
+	go func() {
+		err = doWork()
+	}()
+
+	go printNumbers()
+	go printLetters()
+	time.Sleep(2 * time.Second)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Work completed successfully")
+	}
+
+}
+
+func sayHello() {
+	time.Sleep(1 * time.Second)
+	fmt.Println("Hello from Goroutine")
+}
+
+func printNumbers() {
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+func printLetters() {
+	for _, letter := range "abcde" {
+		fmt.Println(string(letter))
+		time.Sleep(200 * time.Millisecond)
+	}
+}
+
+func doWork() error {
+	time.Sleep(1 * time.Second)
+	return fmt.Errorf("an error occured in doWork.")
+}
